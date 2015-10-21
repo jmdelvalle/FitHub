d39b81b829f9cc6941e1bec43dac612ecb5e3048
henrypl95 <henrypl360@gmail.com>
2015-10-21 14:56:04 -0400

    added user model and fb oauth

--- FitHub/config/application.rb
+++ FitHub/config/application.rb
@@ -31,5 +31,6 @@ module FitHub
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.web_console.whitelisted_ips = '96.224.241.215'
   end
 end
