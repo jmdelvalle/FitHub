98abbecdc548455c9127069a9c86d22c8f32ef92
henrypl95 <henrypl360@gmail.com>
2015-10-27 15:50:02 -0400

    users can search for a gym

--- FitHub/config/routes.rb
+++ FitHub/config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   get 'my_workouts', to: 'workouts#my_workouts'
 
   get 'gyms', to: 'gyms#index'
-  post 'results', to: 'gyms#show'
+  post 'results', to: 'gyms#results'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
