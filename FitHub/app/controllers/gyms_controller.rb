c7ad2b0301e39afbd5ac34c03bda04b59ebfc98e
henrypl95 <henrypl360@gmail.com>
2015-10-27 16:15:15 -0400

    fixed a mistake

--- FitHub/app/controllers/gyms_controller.rb
+++ FitHub/app/controllers/gyms_controller.rb
@@ -2,7 +2,7 @@ class GymsController < ApplicationController
   def index
   end
 
-  def show
+  def results
     search = params[:keyword]
     @city = params[:city]
     api_key = "AIzaSyBNlEoTVzUn-VAoNUeUOSxWt5n40z8DaME"
